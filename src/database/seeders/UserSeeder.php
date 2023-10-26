fd9a2474cc6e712c92437980c43dd196157a6236
moritsu-san <next2the3trees@gmail.com>
2023-10-26 17:15:06 +0900

    ✨ databaseの.gitignoreから/seedersを削除

---
+++ src/database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'username' => 'test',
+            'name' => 'テスト君',
+            'auth_type' => 'MAIL',
+            'email' => 'test@gmail.com',
+            'password' => Hash::make('test1111'),
+        ]);
+    }
+}
