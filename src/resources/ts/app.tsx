77dc789b2a7ef24cacdedea1b51e57f986a43079
moritsu-san <next2the3trees@gmail.com>
2023-10-31 02:13:04 +0900

    🐛baseURLを本番環境用に修正

--- src/resources/ts/app.tsx
+++ src/resources/ts/app.tsx
@@ -15,7 +15,7 @@ import {
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Axios from "axios";
-Axios.defaults.baseURL = "http://localhost:3000/";
+Axios.defaults.baseURL = "https://fumefume.com/";
 
 import Login from "./containers/pages/Login";
 import NotFound from "./components/pages/NotFound";
